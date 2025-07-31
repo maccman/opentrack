df4bd38d6a3ff5ca9288bbb08b0f92f5b9dd0995
Alex MacCaw <alex@alexmaccaw.com>
2025-07-30 23:06:20 -0500

    Remove .prettierrc.json configuration file and update ESLint configuration to use eslint-define-config. Add eslint-define-config as a dependency in package.json and pnpm-lock.yaml.

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,7 @@
-import { defineESLintConfig } from '@ocavue/eslint-config'
+// Ocavue default config isn't working for some reason
+import { defineConfig } from 'eslint-define-config'
 
-export default defineESLintConfig({
+export default defineConfig({
   ignores: ['**/*.md', '.nitro/'],
   rules: {
     // Require curly braces for all control statements
